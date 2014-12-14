a22cbc0a9db28e6dee83ebc9fe61a8528af3c62e
Yi-Yin Wang <acatwang@gmail.com>
2014-12-14 16:34:09 -0500

    Replace table with SlickGrid to support sorting and reordering

--- static/js/myparcoords.js
+++ static/js/myparcoords.js
@@ -9,12 +9,88 @@ var parcoords = d3.parcoords()("#parcoord")
   .alpha(0.4);
 
 // load csv file and create the chart
-d3.csv('static/data/doodle_data_10x10.csv', function(data) {
+d3.csv('static/data/doodle_data_v2.csv', function(data) {
   parcoords
     .data(data)
     .render()
     .brushMode("1D-axes")  // enable brushing
     .reorderable(); // enable reordering
+  
+
+  // slickgrid needs each data element to have an id
+  data.forEach(function(d,i) { d.id = d.id || i; });
+
+  // setting up grid
+  var column_keys = d3.keys(data[0]);
+  var columns = column_keys.map(function(key,i) {
+    return {
+      id: key,
+      name: key,
+      field: key,
+      sortable: true
+    }
+  });
+
+  var options = {
+    enableCellNavigation: true,
+    enableColumnReorder: false,
+    multiColumnSort: false
+  };
+
+  var dataView = new Slick.Data.DataView();
+  var grid = new Slick.Grid("#grid", dataView, columns, options);
+  var pager = new Slick.Controls.Pager(dataView, grid, $("#pager"));
+
+  // wire up model events to drive the grid
+  dataView.onRowCountChanged.subscribe(function (e, args) {
+    grid.updateRowCount();
+    grid.render();
+  });
+  dataView.onRowsChanged.subscribe(function (e, args) {
+    grid.invalidateRows(args.rows);
+    grid.render();
+  });
+  // column sorting
+  var sortcol = column_keys[0];
+  var sortdir = 1;
+  function comparer(a, b) {
+    var x = a[sortcol], y = b[sortcol];
+    return (x == y ? 0 : (x > y ? 1 : -1));
+  }
+  
+  // click header to sort grid column
+  grid.onSort.subscribe(function (e, args) {
+    sortdir = args.sortAsc ? 1 : -1;
+    sortcol = args.sortCol.field;
+    if ($.browser.msie && $.browser.version <= 8) {
+      dataView.fastSort(sortcol, args.sortAsc);
+    } else {
+      dataView.sort(comparer, args.sortAsc);
+    }
+  });
+  // highlight row in chart
+  grid.onMouseEnter.subscribe(function(e,args) {
+    var i = grid.getCellFromEvent(e).row;
+    var d = parcoords.brushed() || data;
+    parcoords.highlight([d[i]]);
+  });
+  grid.onMouseLeave.subscribe(function(e,args) {
+    parcoords.unhighlight();
+  });
+  // fill grid with data
+  gridUpdate(data);
+  // update grid on brush
+  parcoords.on("brush", function(d) {
+    gridUpdate(d);
+  });
+  function gridUpdate(data) {
+    dataView.beginUpdate();
+    dataView.setItems(data);
+    dataView.endUpdate();
+  };
+  
+  /* The following are using example from
+   * https://github.com/syntagmatic/parallel-coordinates/blob/master/examples/table.html
   
   // create data table, row hover highlighting
   var grid = d3.divgrid();
@@ -38,4 +114,6 @@ d3.csv('static/data/doodle_data_10x10.csv', function(data) {
         "mouseout": parcoords.unhighlight
       });
   });
-});
+
+*/
+}); // End of csv reading
